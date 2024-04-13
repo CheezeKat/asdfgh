53ed29b3d13ed25b95b700b49e82aa3868d0ea0b
CheezeKat <166929817+CheezeKat@users.noreply.github.com>
2024-04-13 22:36:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,40 @@
+WIDTH = 1000
+HEIGHT = 1000
+FPS = 60
+
+#Layers
+PLAYER = 2
+OBSTACLE = 1
+
+#Tilesizes
+TILESIZE = 80
+PLW = 50
+PLH = 70
+
+#RGB
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 255, 0)
+
+#PLAYER CONFIG
+SPEED = 4
+
+#TILEMAP
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'BB                BB',
+
+    'BB                BB',
+    'BB        P       BB',
+    'BB                BB',
+    'BB                BB',
+
+    'BB                BB',
+    'BB     BB    BB   BB',
+    'BB                BB',
+    'BB                BB',
+    'BB                BB',
+    'BBBBBBBBBBBBBBBBBBBB',
+
+]
